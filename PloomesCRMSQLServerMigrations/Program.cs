1989b580ac5c866744baab2710b0afca7c364b7d
Fellipe Sena <fellipe.sena@ploomes.com>
2022-06-24 16:01:46 -0300

    Add QA builder

--- PloomesCRMSQLServerMigrations/Program.cs
+++ PloomesCRMSQLServerMigrations/Program.cs
@@ -21,18 +21,23 @@ foreach(var item in options.Databases.Select((value, i) => new { i, value }))
 int selectedDatabase = Convert.ToInt32(Console.ReadLine());
 
 Func<string, bool> generalFilter = scriptName => scriptName.StartsWith("PloomesCRMSQLServerMigrations.Scripts.", StringComparison.OrdinalIgnoreCase);
-var generalUpgrader = DbUpUtilities.CreateUpgrader(options.Databases[selectedDatabase].ConnectionString, generalFilter);
+UpgradeEngine generalUpgrader = DbUpUtilities.CreateUpgrader(options.Databases[selectedDatabase].ConnectionString, generalFilter);
 
 Func<string, bool> procedureFilter = scriptName => scriptName.StartsWith("PloomesCRMSQLServerMigrations.Procedures.", StringComparison.OrdinalIgnoreCase);
-var procedureUpgrader = DbUpUtilities.CreateUpgraderWithNullJournal(options.Databases[selectedDatabase].ConnectionString, procedureFilter);
-
-Func<string, bool> specificFilter = scriptName => scriptName.StartsWith("PloomesCRMSQLServerMigrations." + options.Databases[selectedDatabase].Name, StringComparison.OrdinalIgnoreCase);
-var specificUpgrader = DbUpUtilities.CreateUpgrader(options.Databases[selectedDatabase].ConnectionString, specificFilter);
+UpgradeEngine procedureUpgrader = DbUpUtilities.CreateUpgraderWithNullJournal(options.Databases[selectedDatabase].ConnectionString, procedureFilter);
 
 Func<string, bool> viewFilter = scriptName => scriptName.StartsWith("PloomesCRMSQLServerMigrations.Views.", StringComparison.OrdinalIgnoreCase);
-var viewUpgrader = DbUpUtilities.CreateUpgraderWithNullJournal(options.Databases[selectedDatabase].ConnectionString, viewFilter);
+UpgradeEngine viewUpgrader = DbUpUtilities.CreateUpgraderWithNullJournal(options.Databases[selectedDatabase].ConnectionString, viewFilter);
 
 DbUpUtilities.ExecuteUpgrader(generalUpgrader);
 DbUpUtilities.ExecuteUpgrader(procedureUpgrader);
-DbUpUtilities.ExecuteUpgrader(specificUpgrader);
+
+string databaseName = options.Databases[selectedDatabase].Name;
+if (databaseName != "QA")
+{
+    Func<string, bool> specificFilter = scriptName => scriptName.StartsWith("PloomesCRMSQLServerMigrations." + databaseName, StringComparison.OrdinalIgnoreCase);
+    UpgradeEngine specificUpgrader = DbUpUtilities.CreateUpgrader(options.Databases[selectedDatabase].ConnectionString, specificFilter);
+    DbUpUtilities.ExecuteUpgrader(specificUpgrader);
+}
+
 DbUpUtilities.ExecuteUpgrader(viewUpgrader);
